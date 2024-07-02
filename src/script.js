a6d6569066b9d559fff5d5ec00ce33f3f677493b
washington maina <103877612+washington254@users.noreply.github.com>
2024-07-02 08:15:12 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -17,7 +17,7 @@ import * as dat from 'lil-gui';
 const gui = new dat.GUI();
 const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xf0f0f0);
+scene.background = new THREE.Color(0x232323);
 
 
 // Loaders
